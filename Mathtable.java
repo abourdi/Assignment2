f446eb7c475d3c75c71efa2af54a5b5b4dcca7aa
amine <amine.bourdi@gmail.com>
2014-09-24 14:21:36 -0400

    initial'

---
+++ Mathtable.java
@@ -0,0 +1,25 @@
+
+public class Mathtable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int num1 = 1 % 2;
+	System.out.println(num1);
+	double num2 = 1.0/2;
+	System.out.println(num2);
+	int num3 = 5 + 7 / 2;
+	System.out.println(num3);
+	String num4 = ("Beat" + ' ' + "Army");
+	System.out.println(num4);
+	int num5 = 6 + 13 /5 - 35 % 3;
+	System.out.println(num5);
+	double num6 = 3.5 * (5/4);
+	System.out.println(num6);
+	double num7 = (3.5 * 5) /4;
+	System.out.println(num7);
+	
+	
+	}
+
+}
